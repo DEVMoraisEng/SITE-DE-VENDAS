213d97d5d199e691bfd364b65553cdae127c879f
DEVMoraisEng <adm@moraisengenhariaeconstrucao.com>
2026-06-01 11:38:02 -0300

    Add files via upload

--- fetch_notion.py
+++ fetch_notion.py
@@ -138,7 +138,7 @@ if __name__ == "__main__":
         "TB":  "TERRABELA CERRADO",
         "RR":  "RAVENA",
         "RPB": "PQ DOS BURITIS",
-        "J":   "JOÃO BRAZ 2",
+        "J":   "JOÃO BRAZ",
         "I":   "IPANEMA",
         "IT":  "ITAIPU",
         "A":   "ACROPOLE",
